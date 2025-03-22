40f076571905b646fb3616645118d942e2eed5e7
Daniil Virin <39dv@mail.ru>
2025-03-22 13:25:10 +0300

    First commit

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+   <div class="main">
+      Hello world
+      <font-awesome-icon :icon="'circle'"></font-awesome-icon>
+      <router-view></router-view>
+   </div>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+      }
+   },
+   components: {
+   }
+}
+</script>
